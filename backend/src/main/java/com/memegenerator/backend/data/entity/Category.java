ca67a6557503f5c7e43db614650ebfac6bce16fd
Thomas Visser <t.visser@salvux.com>
2021-03-09 16:51:04 +0100

    Adds frontend & backend models conforming to designed ERD

---
+++ backend/src/main/java/com/memegenerator/backend/data/entity/Category.java
@@ -0,0 +1,17 @@
+package com.memegenerator.backend.data.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Getter
+@Setter
+public class Category extends BaseEntity {
+
+    @Column(name = "title", nullable = false)
+    @NotNull
+    public String title;
+}
