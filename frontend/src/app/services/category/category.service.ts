7fd47d0df5da7ee93715c72c3f4b2330096ff074
codeaur <cornevisser0808@gmail.com>
2021-05-09 21:56:23 +0200

    Fix build?

--- frontend/src/app/services/category/category.service.ts
+++ frontend/src/app/services/category/category.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
-import { Category } from "src/app/models/category";
+import { Category } from "../../../app/models/category";
 
 
 @Injectable({
